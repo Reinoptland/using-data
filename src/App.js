f87d9d81cbca3707bd1fadd070ecc5dcd46a4a9d
Reinoptland <reinoptland@gmail.com>
2022-07-15 10:02:25 +0200

    Cancel promise by returning cleanup function from useEffect

--- src/App.js
+++ src/App.js
@@ -10,13 +10,27 @@ import "./App.css";
 
 function useAdvice() {
   const [apiStatus, setApiStatus] = useState({
-    query: "love",
+    query: "",
     data: [],
     loading: false,
     error: "",
   });
 
+  const debouncedChangeHandler = debounce(
+    (e) => changeHandler(e.target.value),
+    500
+  );
+
   useEffect(() => {
+    const logQuery = () => console.log(apiStatus.query);
+    window.addEventListener("keydown", logQuery);
+
+    return () => window.removeEventListener("keydown", logQuery);
+  }, [apiStatus.query]);
+
+  useEffect(() => {
+    let controller = new AbortController();
+
     async function fetchAdvice() {
       setApiStatus((previousStatus) => {
         return { ...previousStatus, loading: true, error: "" };
@@ -24,7 +38,10 @@ function useAdvice() {
 
       try {
         const response = await fetch(
-          `https://deelay.me/500/https://api.adviceslip.com/advice/search/${apiStatus.query}`
+          `https://deelay.me/2000/https://api.adviceslip.com/advice/search/${apiStatus.query}`,
+          {
+            signal: controller.signal,
+          }
         );
 
         if (response.status === 404) {
@@ -55,6 +72,17 @@ function useAdvice() {
           return { ...previousStatus, loading: false, data: advice.slips };
         });
       } catch (error) {
+        if (error.name === "AbortError") {
+          return setApiStatus((previousStatus) => {
+            return {
+              ...previousStatus,
+              loading: false,
+              data: [],
+              error: "",
+            };
+          });
+        }
+
         return setApiStatus((previousStatus) => {
           return {
             ...previousStatus,
@@ -67,6 +95,10 @@ function useAdvice() {
     }
 
     fetchAdvice();
+
+    return () => {
+      controller.abort();
+    };
   }, [apiStatus.query]);
 
   function changeHandler(newQuery) {
@@ -75,23 +107,22 @@ function useAdvice() {
     });
   }
 
-  return { apiStatus, changeHandler };
+  return { apiStatus, changeHandler, debouncedChangeHandler };
 }
 
 function App() {
   const {
     apiStatus: { error, loading, data },
-    changeHandler,
+    debouncedChangeHandler,
   } = useAdvice();
 
-  const debouncedHandler = debounce((e) => changeHandler(e.target.value), 800);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Get wise with advice</h1>
         <label htmlFor="query">
           What do you want advice about?
-          <input name="query" onChange={debouncedHandler} />
+          <input name="query" onChange={debouncedChangeHandler} />
         </label>
         {loading && <img src={logo} className="App-logo" alt="logo" />}
         {error && <p style={{ color: "red" }}>{error}</p>}

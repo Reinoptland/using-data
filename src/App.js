5f48ba38be7bf5b668d3df3a4960228ed68f3051
Reinoptland <reinoptland@gmail.com>
2022-07-15 09:36:30 +0200

    Debounce search input

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function useAdvice() {
 
       try {
         const response = await fetch(
-          `https://api.adviceslip.com/advice/search/${apiStatus.query}`
+          `https://deelay.me/500/https://api.adviceslip.com/advice/search/${apiStatus.query}`
         );
 
         if (response.status === 404) {
@@ -80,21 +80,18 @@ function useAdvice() {
 
 function App() {
   const {
-    apiStatus: { query, error, loading, data },
+    apiStatus: { error, loading, data },
     changeHandler,
   } = useAdvice();
 
+  const debouncedHandler = debounce((e) => changeHandler(e.target.value), 800);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Get wise with advice</h1>
         <label htmlFor="query">
           What do you want advice about?
-          <input
-            name="query"
-            value={query}
-            onChange={(e) => changeHandler(e.target.value)}
-          />
+          <input name="query" onChange={debouncedHandler} />
         </label>
         {loading && <img src={logo} className="App-logo" alt="logo" />}
         {error && <p style={{ color: "red" }}>{error}</p>}
